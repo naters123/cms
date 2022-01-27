85983549dbc25bfc738570b178f0436284c94d73
naters123 <65196090+naters123@users.noreply.github.com>
2022-01-26 17:17:32 -0700

    week 4
    week 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
-
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document("1","Mario","Super Mario, the mascot of Nintendo","*dummy url*",[]),
+    new Document("2","Luigi","Super Mario's brother","*dummy url*",[]),
+    new Document("3","Peach","The princess of the Mushroom Kingdom","*dummy url*",[]),
+    new Document("4","Yoshi","A friendly green dinosaur","*dummy url*",[]),
+    new Document("5","Bowser","The main antagonist of the Mario Bros franchise","*dummy url*",[])
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
